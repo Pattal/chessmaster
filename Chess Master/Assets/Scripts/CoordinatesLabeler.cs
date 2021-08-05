5b8d02cfb3ebcbbaab8ad72737edad2db797e532
Pattal <56590487+Pattal@users.noreply.github.com>
2021-08-05 18:51:01 +0200

    Create Chess Board and labeling system

---
+++ Chess Master/Assets/Scripts/CoordinatesLabeler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+public class CoordinatesLabeler : MonoBehaviour
+{
+    TextMeshPro Label;
+    Vector2Int Coordiantes;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Label = GetComponent<TextMeshPro>();
+        DisplayCooardinates();
+        ChangeName();
+        Label.enabled = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DisplayCooardinates()
+    {
+        Coordiantes.x = (int) (transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        Coordiantes.y = (int) (transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        Label.text = Coordiantes.x + "," + Coordiantes.y;
+    }
+
+    private void ChangeName()
+    {
+        transform.parent.name = Coordiantes.ToString();
+    }
+}
